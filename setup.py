a6ef64a0662abf4e156ec7d9dc1d5c191ce08134
Boyan Angelov <boyan.angelov@gmail.com>
2019-04-07 18:09:05 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='exmap',
+      version='0.1',
+      description='Explainable maps',
+      url='http://github.com/boyanangelov/test',
+      author='Boyan Angelov',
+      author_email='boyan.angelov@gmail.com',
+      license='MIT',
+      packages=['exmap'],
+      zip_safe=False)
